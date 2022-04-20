6992ba1378ff6dd2d1cb483079dabb00cc85c88a
Sebastian Kalinowski <sebastian@kalinowski.eu>
2022-04-20 14:20:56 +0200

    Run linters on whole project

--- starwars_explorer/wsgi.py
+++ starwars_explorer/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'starwars_explorer.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "starwars_explorer.settings")
 
 application = get_wsgi_application()
