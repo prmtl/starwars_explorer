effcf3fe041b79212a86ec2f427d70354acb5ef5
Sebastian Kalinowski <sebastian@kalinowski.eu>
2022-04-20 21:09:13 +0200

    Add noop fetch endpoint

--- explorer/urls.py
+++ explorer/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CollectionDetailView, CollectionListView
+from .views import CollectionDetailView, CollectionFetchView, CollectionListView
 
 urlpatterns = [
     path("", CollectionListView.as_view(), name="collection-list"),
+    path("fetch/", CollectionFetchView.as_view(), name="collection-fetch"),
     path("<uuid:pk>/", CollectionDetailView.as_view(), name="collection-detail"),
 ]
--- explorer/views.py
+++ explorer/views.py
@@ -1,3 +1,4 @@
+from django.views.generic.base import RedirectView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
@@ -11,3 +12,11 @@ class CollectionListView(ListView):
 
 class CollectionDetailView(DetailView):
     model = Collection
+
+
+class CollectionFetchView(RedirectView):
+    permanent = False
+    pattern_name = "collection-list"
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
