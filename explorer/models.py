de26a58fde931b003572512d88427e939ad18e2e
Sebastian Kalinowski <sebastian@kalinowski.eu>
2022-04-20 19:00:35 +0200

    Add model to store fetched collections

--- explorer/models.py
+++ explorer/models.py
@@ -1,3 +1,18 @@
-from django.db import models  # noqa: F401
+import uuid
 
-# Create your models here.
+from django.db import models
+
+
+# NOTE: it is better to have dates of creation/modification for all imporant models
+class TimestampedModel(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at"]
+
+
+class CollectionModel(TimestampedModel):
+    # NOTE: this is to avoid enumeration of records (but not really a problem here...)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
